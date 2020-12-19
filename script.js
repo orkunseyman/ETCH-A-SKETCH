700b7c952ce8f5108c34c4e8a9a4569574e0b304
Orkun Seyman <orkunseyman@gmail.com>
2020-12-19 18:33:17 +0300

    box added, mouseover event added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const main = document.querySelector('#main');
 let cell = [];
-for(let i = 0; i<1000; i++) {
+for(let i = 0; i<1500; i++) {
     
     cell[i] = document.createElement('div');
     cell[i].classList.add('cell');
@@ -8,7 +8,7 @@ for(let i = 0; i<1000; i++) {
     cell[i].style = 'background-color: rgba(255, 255, 255, 1)';
      //redundant, but deals with override of css styling
      cell[i].addEventListener('mouseover',function () {
-        cell[i].style = `background-color: rgba(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255},${Math.random()*255})`;
+        cell[i].style = `background-color: rgba(${Math.random()*150+100},${Math.random()*150+100},${Math.random()*150+100},${Math.random()*150+100})`;
       })
     main.appendChild(cell[i]);
   }
