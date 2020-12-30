675b7b49f26c65f7ba23028f2bace1ee64b66994
Orkun Seyman <orkunseyman@gmail.com>
2020-12-30 21:14:30 +0300

    clear button added

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const main = document.querySelector('#main');
+const clear = document.querySelector('#clear');
 let cell = [];
-for(let i = 0; i<1500; i++) {
-    
-    cell[i] = document.createElement('div');
-    cell[i].classList.add('cell');
-    
-    cell[i].style = 'background-color: rgba(255, 255, 255, 1)';
-     //redundant, but deals with override of css styling
-     cell[i].addEventListener('mouseover',function () {
-        cell[i].style = `background-color: rgba(${Math.random()*150+100},${Math.random()*150+100},${Math.random()*150+100},${Math.random()*150+100})`;
-      })
-    main.appendChild(cell[i]);
-  }
+for (let i = 0; i < 399; i++) {
 
-   
+  cell[i] = document.createElement('div');
+  cell[i].classList.add('cell');
+  cell[i].style = 'background-color: rgba(255, 255, 255, 1)';
+  cell[i].addEventListener('mouseover', function () {
+    cell[i].style = `background-color: rgba(${Math.random() * 150 + 100},${Math.random() * 150 + 100},${Math.random() * 150 + 100},${Math.random() * 150 + 100})`;
+  })
+  main.appendChild(cell[i]);
+}
+clear.addEventListener('click', function () {
+  for (let i = 0; i < 1500; i++) {
+    cell[i].style = 'background-color: rgba(255, 255, 255, 1)';
+  }
+})
+
